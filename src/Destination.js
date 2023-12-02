067e054912f32f607c31d993d4c8b8affacb73b1
promdiGamer <al_jager1983@yahoo.com>
2023-12-03 00:58:46 +0800

    trying to load image for destination

--- src/Destination.js
+++ src/Destination.js
@@ -1,21 +1,57 @@
 import moon from "./assets/destination/image-moon.png"
+import React, { useState, useEffect } from 'react';
+
 const Destination = () => {
+
+    const [destinations, setDestinations] = useState([]);
+    const [d, setD] = useState(0);
+    const con = (e) => setD(destinations[e.target.id]);
+    const img = d.images.png
+    useEffect(() => {
+        const abortCont = new AbortController();
+        fetch('http://localhost:8000/destinations',  {signal : abortCont.signal})
+        .then(res => {
+        if(!res.ok){        //error throwing
+            throw Error('could not fetch the data for that resource');
+          }
+          return  res.json();
+        })
+        .then(data => {
+            setDestinations(data);
+            
+        })
+        .catch(err => {
+            console.log(err.message);
+        })
+        return () => abortCont.abort();
+    }, []);
+    
     return ( 
         <div className="destination">
             <div className="destination-left">
                 <h1>01 PICK YOUR DESTINATION</h1>
-                <img src={moon} alt="bright side of moon" />
+                <img src={`${img}`} alt="bright side of moon" />
             </div>
             <div className="destination-right">
                 <div className="destination-links">
-                    <h2>Moon</h2>
-                    <h2>Mars</h2>
-                    <h2>Europa</h2>
-                    <h2>Titan</h2>
+                    <h2 onClick={con} id="0">Moon</h2>
+                    <h2 onClick={con} id="1">Mars</h2>
+                    <h2 onClick={con} id="2">Europa</h2>
+                    <h2 onClick={con} id="3">Titan</h2>
                 </div>
+                <div className="destination-name">
+                <h2>{d.name}</h2> 
+                <p>{d.description}</p> 
+                </div>
+                <div className="description"></div>
+                <hr />
+                <div className="destination-stats"></div>
             </div>
         </div>
      );
 }
  
 export default Destination;
+
+//npx json-server --watch data.json --port 8000
+//above line is to run json server
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import logo from "./assets/shared/logo.svg";
 
 const Navbar = () => {
+
+    
     return ( 
         <div>
             <div className="logo">
