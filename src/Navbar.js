d137696f325e59a1f8a0887cc4d244a9bc8f80c6
unknown <al_jager1983@yahoo.com>
2023-12-15 18:16:44 +0800

    working on navbar to be responsive

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,52 @@
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+import { Link, NavLink } from "react-router-dom";
 import logo from "./assets/shared/logo.svg";
 import burger from "./assets/shared/icon-hamburger.svg";
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState();
+    const [navStyle, setNavStyle]  = useState();
+    const [burgStyle, setBurgStyle] = useState();
+    const docWidth = document.documentElement.clientWidth;
+    
+    useEffect(() => {
+        if(docWidth >= 377) {
+            setNavStyle("flex");
+            setBurgStyle("none");
+        } else {
+            setNavStyle("none")
+            setBurgStyle("flex");
+        }
+    }, [toggle])
 
+    const hide = () => {
+        // setNavStyle("flex");
+        setNavStyle("flex");
+        setBurgStyle("none");
+        
+    }
+
+    const clickToggle = () => {
+        setToggle(x => !x);
+    }
     
     return ( 
         <div>
+            <div className='docWidth'>{docWidth}</div>
+
             <div className="logo">
                 <img src={logo} alt="star-logo" className="logo-icon" />  
                 <hr className="hr-nav"/>
             </div>
        
-            <nav className="navbar">
-                <Link to="/" className="navitems" id="link-home"><span className="bold">00</span><span>HOME</span></Link>
-                <Link to="/destination"  className="navitems" id="link-destination"><span className="bold">01</span><span>DESTINATION</span></Link>
-                <Link to="/crew"  className="navitems" id="link-crew"><span className="bold">02</span><span>CREW</span></Link>
-                <Link to="/technology"  className="navitems" id="link-tech"><span className="bold">03</span><span>TECHNOLOGY</span></Link>
+            <nav className="navbar" style={{display: `${navStyle}` }}>
+                <Link to="/" className="navitems" id="link-home" onClick={clickToggle}><span className="bold">00</span><span>HOME</span></Link>
+                <Link to="/destination"  className="navitems" id="link-destination" onClick={clickToggle}><span className="bold">01</span><span>DESTINATION</span></Link>
+                <Link to="/crew"  className="navitems" id="link-crew" onClick={clickToggle}><span className="bold" >02</span><span>CREW</span></Link>
+                <Link to="/technology"  className="navitems" id="link-tech" onClick={clickToggle}><span className="bold" >03</span><span>TECHNOLOGY</span></Link>
             </nav>
-            <img src={burger} alt="burger icon" className="burger"/>
+            <img src={burger} alt="burger icon" className="burger" onClick={hide} style={{display: `${burgStyle}` }}/>
         </div>
      );
 }
