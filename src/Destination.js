5843eb934193bbe356adc629658df99e72ae6015
promdiGamer <al_jager1983@yahoo.com>
2024-01-10 18:33:23 +0800

    troubleshooting for live page

--- src/Destination.js
+++ src/Destination.js
@@ -17,10 +17,11 @@ const Destination = () => {
         setDDescrip(d.description);
         setDDistance(d.distance);
         setDTravelTime(d.travel);
+        console.log(d);
     };
     useEffect(() => {
         const getData = () => {
-        fetch('data.json')
+        fetch('http://localhost:8000/destinations')
         .then(res => {
         if(!res.ok){        //error throwing
             throw Error('could not fetch the data for that resource');
@@ -28,12 +29,12 @@ const Destination = () => {
           return  res.json();
         })
         .then(data => {
-            setDestinationsData(data.destinations);
-            setDName(data.destinations[0].name);
-            setDDescrip(data.destinations[0].description);
-            setDDistance(data.destinations[0].distance);
-            setDTravelTime(data.destinations[0].travel);
-            console.log("data has been loaded");
+            setDestinationsData(data);
+            setDName(data[0].name);
+            setDDescrip(data[0].description);
+            setDDistance(data[0].distance);
+            setDTravelTime(data[0].travel);
+            // console.log(data[0].name);
             // setIsPending(false);
         })
         .catch(err => {
